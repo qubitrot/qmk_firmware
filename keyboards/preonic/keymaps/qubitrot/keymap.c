cfb63b76896a933771ec4ddf4012bd1bafa12ae8
Keith <keith@qubitrot.org>
2020-12-20 22:05:24 -0800

    Add media keys

--- keyboards/preonic/keymaps/qubitrot/keymap.c
+++ keyboards/preonic/keymaps/qubitrot/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt |       | GUI  |Raise |    Space    |Lower |      |      |      |      |
+ * | Ctrl | Alt  | Alt  | GUI  |Raise |    Space    |Lower | Prev | Next |Pause |LRCTL |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid( \
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
-  KC_LCTL, KC_LALT, KC_LALT, KC_LGUI, RAISE,   KC_SPC,  KC_SPC,  LOWER,   _______, _______, _______, LR_CTRL  \
+  KC_LCTL, KC_LALT, KC_LALT, KC_LGUI, RAISE,   KC_SPC,  KC_SPC,  LOWER,   KC_MPRV, KC_MNXT, KC_MPLY, LR_CTRL  \
 ),
 
 /* Lower
